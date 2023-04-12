cdd4099776c64ca529bc1eed4b6448ab6e2b5d1f
1franken <francoexequiel_garcia@hotmail.com>
2023-04-12 11:34:38 -0300

    ejercicios unidad1

---
+++ Unidad1/ejemplo.py
@@ -0,0 +1,12 @@
+a=4
+b=2
+
+print(f"{a} más {b} es igual a {a+b}")
+
+suma = a + b
+resta = a - b
+division = a / b
+multiplicacion = a * b
+raizCuadradaA = a ** 0.5
+raizCuadradaB = b ** 0.5
+potencia = a ** b
